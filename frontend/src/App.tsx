34dcedd025a21fd71badba4bf6cd9c9b46ca6d84
dany.prasetya <danyprasetya8@gmail.com>
2025-07-15 09:17:03 +0700

    dockerize ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,63 @@
-import './App.css'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 function App() {
 
   return (
-    <>
-      <div>
-        test
+    <div className="w-1/3 mx-auto my-30">
+      <div className="text-3xl font-bold mb-8">
+        Airline Voucher Seat Assignment
       </div>
-    </>
+
+      <form
+        action=""
+        className="grid grid-cols-2 gap-8"
+      >
+        <div>
+          <div className="mb-2">
+            Crew ID
+          </div>
+          <Input placeholder="Input crew id" />
+        </div>
+
+        <div>
+          <div className="mb-2">
+            Crew Name
+          </div>
+          <Input placeholder="Input crew name" />
+        </div>
+
+        <div>
+          <div className="mb-2">
+            Flight Number
+          </div>
+          <Input placeholder="Input flight number" />
+        </div>
+
+        <div>
+          <div className="mb-2">
+            Flight Date
+          </div>
+          <Input placeholder="Input flight date" />
+        </div>
+
+        <div>
+          <div className="mb-2">
+            Aircraft Type
+          </div>
+          <Input placeholder="" />
+        </div>
+
+        <div />
+
+        <Button
+          className="col-span-2"
+          type="submit"
+        >
+          Generate
+        </Button>
+      </form>
+    </div>
   )
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+  esbuild: {
+    target: 'esnext',
+  }
 })
